80ca294715e52259f257da129e84984cc77566ab
anil <anillakhlan2107@gmail.com>
2022-03-09 12:55:45 +0530

    setting up recepie-list array and data binding for recepie list

--- src/app/recepies/recepie-list/recepie-list.component.ts
+++ src/app/recepies/recepie-list/recepie-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Recepie } from '../recepie.model';
 
 @Component({
   selector: 'app-recepie-list',
   templateUrl: './recepie-list.component.html',
-  styleUrls: ['./recepie-list.component.css']
+  styleUrls: ['./recepie-list.component.css'],
 })
 export class RecepieListComponent implements OnInit {
-  recepies =[]
-  constructor() { }
+  recepies: Recepie[] = [
+    new Recepie(
+      'test-recepie',
+      'this is test description for test-recepie',
+      'https://hindi.cdn.zeenews.com/hindi/sites/default/files/2020/09/11/637971-pjimage26.jpg'
+    ),
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
