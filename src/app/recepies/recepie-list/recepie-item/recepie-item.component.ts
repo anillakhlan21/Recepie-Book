35f9360cf97274b25c029748115a761aae44f2ee
anil <anillakhlan2107@gmail.com>
2022-03-12 00:29:35 +0530

    minor changes-passing recepie data from recepie-list to recepie itme using property binding

--- src/app/recepies/recepie-list/recepie-item/recepie-item.component.ts
+++ src/app/recepies/recepie-list/recepie-item/recepie-item.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Recepie } from '../../recepie.model';
 
 @Component({
   selector: 'app-recepie-item',
   templateUrl: './recepie-item.component.html',
-  styleUrls: ['./recepie-item.component.css']
+  styleUrls: ['./recepie-item.component.css'],
 })
 export class RecepieItemComponent implements OnInit {
+  @Input() recepie: Recepie;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(){}
 }
