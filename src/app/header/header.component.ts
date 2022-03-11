9c90508f776e0eb14a79a1c9945225b4b010adec
anil <anillakhlan2107@gmail.com>
2022-03-11 23:19:36 +0530

    navbar feature event binding for recepie and shopping-list

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
@@ -6,4 +6,8 @@ import { Component } from "@angular/core";
 })
 export class HeaderComponent{
     collapsed=true
+    @Output() fireFeature = new EventEmitter<string>();
+    selectedFeature(feature: string){
+        this.fireFeature.emit(feature);
+    }
 }
